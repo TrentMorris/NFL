a4ca06cdc4f721249857ba3504965b484bd6b1d6
TrentMorris <trent.morris@banno.com>
2014-09-14 20:24:30 -0500

    finished tests for current weeks and getting from file

--- src/main/scala/Actors/WeekIndexes.scala
+++ src/main/scala/Actors/WeekIndexes.scala
@@ -25,18 +25,18 @@ object WeekIndexes2014 {
 	val weekTwo = 33
 	val weekThree = 65
 	val weekFour = 97
-	val weekFive = 1
-	val weekSix =1
-	val weekSeven=1
-	val weekEight=1
-	val weekNine=1
-	val weekTen=1
-	val weekEleven=1
-	val weekTwelve=1
-	val weekThirteen=1
-	val weekFourteen=1
-	val weekFifteen=1
-	val weekSixteen=1
-	val weekSeventeen=1
+	val weekFive = 123
+	val weekSix = 153
+	val weekSeven = 183
+	val weekEight = 213
+	val weekNine = 243
+	val weekTen = 269
+	val weekEleven =295
+	val weekTwelve = 323
+	val weekThirteen = 353
+	val weekFourteen = 385
+	val weekFifteen = 417
+	val weekSixteen = 449
+	val weekSeventeen = 481
 }
 
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,14 +8,15 @@ object NFLPredictor {
     val system = ActorSystem("master")
     val master = system.actorOf(Props(new Master()), name = "master")
 
-    val source = Source.fromURL(getClass.getResource("/nfl2013stats.csv"))
-    val statsList = source.mkString.replace("\n",",").split(",").toList.sliding(35,35).toList
-    // val secondSource = Source.fromURL(getClass.getResource("/nfl2014stats.csv"))
-    // val statsList2014 = secondSource.mkString.replace("\n",",").split(",").toList
+    // val source = Source.fromURL(getClass.getResource("/nfl2013stats.csv"))
+    // val statsList = source.mkString.replace("\n",",").split(",").toList.sliding(35,35).toList
 
-    for (game <- statsList){ for (stat <- game) print("\"" + stat + "\", " ); println("\n\n")}
+    val secondSource = Source.fromURL(getClass.getResource("/nfl2014stats.csv"))
+    val statsList2014 = secondSource.mkString.replace("\n",",").split(",").toList.sliding(35,35).toList
 
-    // for (index <- 0 to statsList2014.size) if (statsList2014(index) == "09/11/2014") println(index)
+    // for (game <- statsList){ for (stat <- game) print("\"" + stat + "\", " ); println("\n\n")}
+    // for (index <- 0 to statsList2014.size) if (statsList2014(index)(0) == "12/18/2014") println(index)
+    for (game <- statsList2014){ for (stat<- game)print("\"" + stat + "\","); println("")}
 
     master ! Game("Steelers", "Bengals")
 
